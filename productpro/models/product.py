500b32326b3d4546847132829ce313ce4780be02
santiemanuel <santiemanuel@gmail.com>
2023-06-06 18:10:11 -0300

    Creación de modelos Provider y Product y su migración

---
+++ productpro/models/product.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.IntegerField()
+    stock = models.IntegerField()
+    provider = models.ForeignKey("Provider", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} - {self.price} - {self.stock} - {self.provider}"
